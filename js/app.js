5a12fb71d6fb90972186dd617a2260bc71bf07cc
LIN Yu <lin.yu@elico-corp.com>
2015-10-05 06:32:41 +0000

    lesson 7

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+function makeCartScrollNicely() {
+  var cart = document.querySelector(".cart");
+  Ps.initialize(cart);
+}
+
+
 window.onload = function() {
+  makeCartScrollNicely();
   console.log("page loaded");
 }
